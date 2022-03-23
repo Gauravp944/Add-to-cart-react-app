d9ca96d4a71ecbe2635f9a2a4c9d45f6bd947241
Gaurav <gauravppatkar@gmail.com>
2022-03-23 23:49:59 +0530

    Add to Cart

---
+++ my-app/build/precache-manifest.96c9c4f5c51d17b7b801a0662c405780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31312dadc8f8580a44b6",
+    "url": "/static/css/main.f4bd58b8.chunk.css"
+  },
+  {
+    "revision": "31312dadc8f8580a44b6",
+    "url": "/static/js/main.31312dad.chunk.js"
+  },
+  {
+    "revision": "cb584ef25d7bf1c60e25",
+    "url": "/static/css/1.3cdcda15.chunk.css"
+  },
+  {
+    "revision": "cb584ef25d7bf1c60e25",
+    "url": "/static/js/1.cb584ef2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b8889629392719b14bf96591d0e6888",
+    "url": "/index.html"
+  }
+];
